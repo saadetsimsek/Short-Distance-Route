0fa942d7d6d45b0065d67c70b03dfdcf95b51903
saadetsimsek <saadetsimsek13@gmail.com>
2024-08-03 18:25:58 +0300

    Initial Commit

---
+++ ShortDistanceRoute/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShortDistanceRoute
+//
+//  Created by Saadet Şimşek on 03/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
